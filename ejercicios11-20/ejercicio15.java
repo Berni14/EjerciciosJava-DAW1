db85e8b8e898f6928b20dbb563c992fe80a2938a
Berni14 <m.bebon14@gmail.com>
2025-10-30 10:11:04 +0100

    ejercicio15

--- ejercicios11-20/ejercicio15.java
+++ ejercicios11-20/ejercicio15.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class ejercicio15 {
     public static void armstrong(int numero){
-        int numInicial = numero;
-        int numArmstrong = 0;
-        while(numero >= 0){
-            numArmstrong = (numero % 10); 
-            numero = numero/ 10;
+        int numArmstrong=0;
+        int tamaño= 0;
+        int numAuxiliar = numero; int numInicial= numero;
+        while (numAuxiliar > 0) {
+            numAuxiliar /= 10;
+            tamaño++;
+        }
+
+        numAuxiliar = numero;
+        while (numAuxiliar > 0) {
+            int digito = numAuxiliar % 10;
+            numArmstrong += (int) Math.pow(digito, tamaño);
+            numAuxiliar /= 10;
         }
         System.out.println(numArmstrong);
+        if (numInicial == numArmstrong){
+            System.out.println("Es un numero Armstrong");
+        } else{
+            System.out.println("No es un numero Armstrong");
+        }
     }
     public static void main(String[] args) {
         
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el numero para ver si es un numero armstrong");
+        int numero = sc.nextInt();
+
+        armstrong(numero);
+
+        sc.close();
     }
 }
